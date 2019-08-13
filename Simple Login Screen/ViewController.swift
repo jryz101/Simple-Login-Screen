3f2b35627811e6b8e4d4ac38d0e3a4c755d17d44
JerryTan <jryz101@gmail.com>
2019-08-14 03:48:57 +0800

    2. Link up label and text field IB outlet, and creates loginButton action.

--- Simple Login Screen/ViewController.swift
+++ Simple Login Screen/ViewController.swift
@@ -1,20 +1,26 @@
-//
 //  ViewController.swift
 //  Simple Login Screen
-//
 //  Created by Jerry Tan on 14/08/2019.
 //  Copyright © 2019 Starknet Technologies®. All rights reserved.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField : UITextField!
+    @IBOutlet weak var label     : UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    
+    
+    @IBAction func loginButton(_ sender: UIButton) {
+        
+    }
 }
 
