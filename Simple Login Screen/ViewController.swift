fc1afd959981cfe470e1693ddb46bb3aa2e0e0ec
JerryTan <jryz101@gmail.com>
2019-08-14 03:20:29 +0800

    Initial Commit

---
+++ Simple Login Screen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simple Login Screen
+//
+//  Created by Jerry Tan on 14/08/2019.
+//  Copyright © 2019 Starknet Technologies®. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
