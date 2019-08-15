123d7546601482d2b703d3a67189d5991fdfb0cf
JerryTan <jryz101@gmail.com>
2019-08-15 17:07:13 +0800

    3. Create appDelegate, context persistentContainer, NSFetchRequest, and declare request returnObjectsAsFaults.

--- Simple Login Screen/ViewController.swift
+++ Simple Login Screen/ViewController.swift
@@ -5,9 +5,11 @@
 
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
-
+    
+    //IB outlets
     @IBOutlet weak var textField : UITextField!
     @IBOutlet weak var label     : UILabel!
     
@@ -15,10 +17,25 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //Cast UIApplication delegate | AppDelegate
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        //Create context | The managed object context associated with the main queue (read-only)
+        let context = appDelegate.persistentContainer.viewContext
+        
+        //Create reuqest | A description of search criteria used to retrieve data from a persistent store
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "UsersDatabase")
+        
+        //Return bojects as faults | A Boolean value that indicates whether the objects resulting from a fetch request are faults
+        request.returnsObjectsAsFaults = false
+        
+        
+        
     }
     
     
-    
+    //Login button action
     @IBAction func loginButton(_ sender: UIButton) {
         
     }
